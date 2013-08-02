64bcabfd3de2178d915e62cd925353856596c0a6
Tom MacWright <tom@macwright.org>
2013-08-02 17:18:49 -0400

    Warn on IE9 share.

--- src/gist.js
+++ src/gist.js
@@ -1,4 +1,9 @@
 function saveAsGist(content, callback) {
+
+    if (navigator.appVersion.indexOf('MSIE 9') !== -1 || !window.XMLHttpRequest) {
+        return alert('Sorry, saving and sharing is not supported in IE9 and lower. ' +
+            'Please use a modern browser to enjoy the full featureset of geojson.io');
+    }
     d3.text('data/share.html').on('load', function(sharehtml) {
         var h = new window.XMLHttpRequest();
         h.onload = function() {
