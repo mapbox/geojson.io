d2b2d9b3cdbac1a418c2b08ed9a9de947901ff70
Tom MacWright <tom@macwright.org>
2013-08-02 12:31:28 -0400

    In-popup editing

--- src/index.js
+++ src/index.js
@@ -155,7 +155,7 @@ function geoify(layer) {
 }
 
 function drawCreated(e) {
-    if ('setStyle' in e.layer) e.layer.setStyle(brush);
+    // if ('setStyle' in e.layer) e.layer.setStyle(brush);
     drawnItems.addLayer(e.layer);
     geoify(drawnItems);
 }
@@ -198,6 +198,32 @@ var buttons = d3.select('.buttons')
     });
 
 d3.select(buttons.node()).trigger('click');
+
+map.on('popupopen', function(e) {
+    var sel = d3.select(e.popup._contentNode);
+
+    sel.selectAll('.cancel')
+        .on('click', clickClose);
+
+    sel.selectAll('.save')
+        .on('click', saveFeature);
+
+    function clickClose() {
+        map.closePopup(e.popup);
+    }
+
+    function saveFeature() {
+        var obj = {};
+        sel.selectAll('tr')
+            .each(function() {
+                obj[d3.select(this).selectAll('input')[0][0].value] =
+                    d3.select(this).selectAll('input')[0][1].value;
+            });
+        e.popup._source.feature.properties = obj;
+        map.closePopup(e.popup);
+        refresh();
+    }
+});
 
 function jsonPanel(container) {
     container.html('');
@@ -421,11 +447,13 @@ function showProperties(l) {
     var properties = l.toGeoJSON().properties, table = '';
     for (var key in properties) {
         if (styleProps.indexOf(key) == -1) {
-        table += '<tr><th>' + key + '</th>' +
-            '<td>' + properties[key] + '</td></tr>';
+        table += '<tr><th><input type="text" value="' + key + '" /></th>' +
+            '<td><input type="text" value="' + properties[key] + '" /></td></tr>';
         }
     }
-    if (table) l.bindPopup('<table class="marker-properties">' + table + '</table>');
+    if (table) l.bindPopup('<table class="marker-properties">' + table + '</table>' +
+        '<button class="save">save</button>' +
+        '<button class="cancel">cancel</button>');
 }
 
 function mapFile(gist) {
