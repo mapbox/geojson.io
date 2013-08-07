d7189a4f8958423688ba1b02ef9a718524f232b3
Tom MacWright <tom@macwright.org>
2013-08-06 22:30:53 -0400

    Only request public repos

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 function loginPanel(container) {
-    location.href = 'https://github.com/login/oauth/authorize?client_id=' + client_id + '&scope=gist,repo,public_repo';
+    location.href = 'https://github.com/login/oauth/authorize?client_id=' + client_id + '&scope=gist,public_repo';
 }
 
 loginPanel.init = function(container) {
