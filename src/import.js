aae469fd2bb742af49060b561f23a764c47c3652
Tom MacWright <tom@macwright.org>
2013-08-08 22:55:12 -0400

    Fix more regressions.

--- src/import.js
+++ src/import.js
@@ -35,11 +35,14 @@ function importPanel(container) {
     }
 
     function detectIndentationStyle(f) {
-        var indent = f.split('\n')[1].match(/^(\s*)/)[0];
-        if (indent[0] == '\t') {
+        var lines = f.split('\n');
+        if (lines.length < 2) return 4;
+        var indent = lines[1].match(/^(\s*)/);
+        if (!indent || !indent.length) return 4;
+        if (indent[0][0] == '\t') {
             return '\t';
         } else {
-            return indent.length;
+            return indent[0].length;
         }
     }
 
