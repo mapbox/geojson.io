bac68d033592313e909d19531372640964850297
Tom MacWright <tom@macwright.org>
2013-09-03 19:14:22 -0400

    Handle keybinding in CodeMirror. Fixes #209

--- src/panel/json.js
+++ src/panel/json.js
@@ -1,14 +1,22 @@
-var validate = require('../lib/validate');
-
-CodeMirror.keyMap.tabSpace = {
-    Tab: function(cm) {
-        var spaces = new Array(cm.getOption('indentUnit') + 1).join(' ');
-        cm.replaceSelection(spaces, 'end', '+input');
-    },
-    fallthrough: ['default']
-};
+var validate = require('../lib/validate'),
+    saver = require('../ui/saver.js');
 
 module.exports = function(context) {
+
+    CodeMirror.keyMap.tabSpace = {
+        Tab: function(cm) {
+            var spaces = new Array(cm.getOption('indentUnit') + 1).join(' ');
+            cm.replaceSelection(spaces, 'end', '+input');
+        },
+        'Ctrl-S': saveAction,
+        'Cmd-S': saveAction,
+        fallthrough: ['default']
+    };
+
+    function saveAction() {
+        saver(context);
+        return false;
+    }
 
     function render(selection) {
         var textarea = selection
@@ -26,7 +34,6 @@ module.exports = function(context) {
             lineNumbers: true
         });
 
-        // shush the callback-back
         editor.on('change', validate(changeValidated));
 
         function changeValidated(err, data) {
