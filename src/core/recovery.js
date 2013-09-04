9d75326d536965eae145ec498f780263de32300d
Tom MacWright <tom@macwright.org>
2013-09-04 11:38:43 -0400

    Kill recovery when you switch from local

--- src/core/recovery.js
+++ src/core/recovery.js
@@ -1,15 +1,27 @@
 module.exports = function(context) {
-    d3.select(window).on('unload', function() {
-        if (context.data.get('type') === 'local' && context.data.hasFeatures()) {
-            context.storage.set('recover', context.data.all());
-        }
-    });
+
+    d3.select(window).on('unload', onunload);
+    context.dispatch.on('change', onchange);
 
     if (location.hash !== '#new') {
         var rec = context.storage.get('recover');
         if (rec && confirm('recover your map from the last time you edited?')) {
             context.data.set(rec);
         } else {
+            context.storage.remove('recover');
+        }
+    }
+
+    function onunload() {
+        if (context.data.get('type') === 'local' && context.data.hasFeatures()) {
+            context.storage.set('recover', context.data.all());
+        } else {
+            context.storage.remove('recover');
+        }
+    }
+
+    function onchange() {
+        if (context.data.get('type') !== 'local') {
             context.storage.remove('recover');
         }
     }
