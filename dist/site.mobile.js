46054774f20fcd02435a856b60741d80ca4c71a8
Bryan McBride <mcbride.bryan@gmail.com>
2015-02-12 21:27:08 -0500

    Tabbed popups and style inputs
    Provide HTML5 inputs to help users style features. Uses color type for
    marker-color, stroke & fill, number type for stroke-width, range slider
    for stroke-opacity & fill-opacity, and datalist for marker-size &
    marker-symbol. Falls back to text input for unsupported browsers.

--- dist/site.mobile.js
+++ dist/site.mobile.js
@@ -25243,7 +25243,16 @@ var popup = require('../lib/popup'),
     customHash = require('../lib/custom_hash.js'),
     qs = require('qs-hash'),
     LGeo = require('leaflet-geodesy'),
-    writable = false;
+    writable = false,
+    showStyle = true,
+    maki = '';
+
+d3.json("data/maki.json", function(error, json) {
+    for (i = 0; i < json.length; i++) {
+        console.log(json[i].icon);
+        maki += '<option value="' + json[i].icon + '">';
+    }
+});
 
 module.exports = function(context, readonly) {
 
@@ -25344,9 +25353,72 @@ function bindPopup(l) {
 
     if (!Object.keys(properties).length) properties = { '': '' };
 
+    if (l.feature && l.feature.geometry && writable) {
+        if (l.feature.geometry.type === 'Point') {
+            if (!('marker-color' in properties)) {
+                table += '<tr class="style-row"><th><input type="text" value="marker-color"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                    '<td><input type="color" value="#7E7E7E"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+            }
+            if (!('marker-size' in properties)) {
+                table += '<tr class="style-row"><th><input type="text" value="marker-size"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                    '<td><input type="text" list="marker-size" value="medium"' + (!writable ? ' readonly' : '') + ' /><datalist id="marker-size"><option value="small"><option value="medium"><option value="large"></datalist></td></tr>';
+            }
+            if (!('marker-symbol' in properties)) {
+                table += '<tr class="style-row"><th><input type="text" value="marker-symbol"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                    '<td><input type="text" list="marker-symbol" value=""' + (!writable ? ' readonly' : '') + ' /><datalist id="marker-symbol">' + maki + '</datalist></td></tr>';
+            }
+        }
+        if (l.feature.geometry.type === 'LineString' || l.feature.geometry.type === 'Polygon') {
+            if (!('stroke' in properties)) {
+                table += '<tr class="style-row"><th><input type="text" value="stroke"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                    '<td><input type="color" value="#555555"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+            }
+            if (!('stroke-width' in properties)) {
+                table += '<tr class="style-row"><th><input type="text" value="stroke-width"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                    '<td><input type="number" min="0" step="0.1" value="2"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+            }
+            if (!('stroke-opacity' in properties)) {
+                table += '<tr class="style-row"><th><input type="text" value="stroke-opacity"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                    '<td><input type="range" min="0" max="1" step="0.1" value="1"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+            }
+        }
+        if (l.feature.geometry.type === 'Polygon') {
+            if (!('fill' in properties)) {
+                table += '<tr class="style-row"><th><input type="text" value="fill"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                    '<td><input type="color" value="#555555"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+            }
+            if (!('fill-opacity' in properties)) {
+                table += '<tr class="style-row"><th><input type="text" value="fill-opacity"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                    '<td><input type="range" min="0" max="1" step="0.1" value="0.5"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+            }
+        }
+    }
+
     for (var key in properties) {
-        table += '<tr><th><input type="text" value="' + key + '"' + (!writable ? ' readonly' : '') + ' /></th>' +
-            '<td><input type="text" value="' + properties[key] + '"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+        if ((key == 'marker-color' || key == 'stroke' || key == 'fill') && writable) {
+            table += '<tr class="style-row"><th><input type="text" value="' + key + '"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                '<td><input type="color" value="' + properties[key] + '"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+        }
+        else if (key == 'marker-size' && writable) {
+            table += '<tr class="style-row"><th><input type="text" value="' + key + '"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                '<td><input type="text" list="marker-size" value="' + properties[key] + '"' + (!writable ? ' readonly' : '') + ' /><datalist id="marker-size"><option value="small"><option value="medium"><option value="large"></datalist></td></tr>';
+        }
+        else if (key == 'marker-symbol' && writable) {
+            table += '<tr class="style-row"><th><input type="text" value="' + key + '"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                '<td><input type="text" list="marker-symbol" value="' + properties[key] + '"' + (!writable ? ' readonly' : '') + ' /><datalist id="marker-symbol">' + maki + '</datalist></td></tr>';
+        }
+        else if (key == 'stroke-width' && writable) {
+            table += '<tr class="style-row"><th><input type="text" value="' + key + '"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                '<td><input type="number" min="0" step="0.1" value="' + properties[key] + '"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+        }
+        else if ((key == 'stroke-opacity' || key == 'fill-opacity') && writable) {
+            table += '<tr class="style-row"><th><input type="text" value="' + key + '"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                '<td><input type="range" min="0" max="1" step="0.1" value="' + properties[key] + '"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+        }
+        else {
+            table += '<tr><th><input type="text" value="' + key + '"' + (!writable ? ' readonly' : '') + ' /></th>' +
+                '<td><input type="text" value="' + properties[key] + '"' + (!writable ? ' readonly' : '') + ' /></td></tr>';
+        }
     }
 
     if (l.feature && l.feature.geometry) {
@@ -25356,20 +25428,44 @@ function bindPopup(l) {
                 return total + L.latLng(pair[0][1], pair[0][0])
                     .distanceTo(L.latLng(pair[1][1], pair[1][0]));
             }, 0);
-            info += '<tr><td>meters</td><td>' + total.toFixed(2) +    '</td></tr>';
-            info += '<tr><td>miles</td><td>' + (total / 1609.34).toFixed(2) + '</td></tr>';
+            info += '<tr><td>Meters</td><td>' + total.toFixed(2) + '</td></tr>' +
+                    '<tr><td>Kilometers</td><td>' + (total / 1000).toFixed(2) + '</td></tr>' +
+                    '<tr><td>Feet</td><td>' + (total / 0.3048).toFixed(2) + '</td></tr>' +
+                    '<tr><td>Yards</td><td>' + (total / 0.9144).toFixed(2) + '</td></tr>' +
+                    '<tr><td>Miles</td><td>' + (total / 1609.34).toFixed(2) + '</td></tr>';
         } else if (l.feature.geometry.type === 'Point') {
             info += '<tr><td>Latitude </td><td>' + l.feature.geometry.coordinates[1].toFixed(4) + '</td></tr>' +
                     '<tr><td>Longitude</td><td>' + l.feature.geometry.coordinates[0].toFixed(4) + '</td></tr>';
         } else if (l.feature.geometry.type === 'Polygon') {
-            info += '<tr><td>Area km<sup>2</sup></td><td>' + (LGeo.area(l) / 1000000).toFixed(2) + '</td></tr>';
+          info += '<tr><td>Sq. Meters</td><td>' + (LGeo.area(l)).toFixed(2) + '</td></tr>' +
+                  '<tr><td>Sq. Kilometers</td><td>' + (LGeo.area(l) / 1000000).toFixed(2) + '</td></tr>' +
+                  '<tr><td>Sq. Feet</td><td>' + (LGeo.area(l) / 0.092903).toFixed(2) + '</td></tr>' +
+                  '<tr><td>Acres</td><td>' + (LGeo.area(l) / 4046.86).toFixed(2) + '</td></tr>' +
+                  '<tr><td>Sq. Miles</td><td>' + (LGeo.area(l) / 2589990).toFixed(2) + '</td></tr>';
         }
         info += '</table>';
     }
 
+    var tabs = '<div class="tabs-ui">' +
+                    '<div class="tab">' +
+                        '<input type="radio" id="properties" name="tab-group" checked="true">' +
+                        '<label for="properties">Properties</label>' +
+                        '<div class="content">' +
+                            (writable ? '<input type="checkbox" id="show-style" name="show-style" value="true" checked> Show style properties<br>' : '') +
+                            '<table class="marker-properties">' + table + '</table>' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="tab">' +
+                        '<input type="radio" id="info" name="tab-group">' +
+                        '<label for="info">Info</label>' +
+                        '<div class="content">' +
+                            '<div class="marker-info">' + info + ' </div>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>';
+
     var content = '<div class="clearfix">' +
-        '<div class="marker-info">' + info + ' </div>' +
-        '<div class="marker-properties-limit"><table class="marker-properties">' + table + '</table></div>' +
+        '<div>' + tabs + '</div>' +
         (writable ? '<br /><div class="clearfix col12">' +
             '<div class="buttons-joined fl"><button class="add major">add row</button> ' +
             '<button class="save major">save</button> ' +
@@ -25381,6 +25477,22 @@ function bindPopup(l) {
         maxWidth: 500,
         maxHeight: 400
     }, l).setContent(content));
+
+    l.on('popupopen', function(e){
+        if (showStyle === false) {
+            d3.select('#show-style').property('checked', false);
+              d3.selectAll('.style-row').style('display','none');
+        }
+        d3.select('#show-style').on('click', function() {
+            if (this.checked) {
+                showStyle = true;
+                d3.selectAll('.style-row').style('display','');
+            } else {
+                showStyle = false;
+                d3.selectAll('.style-row').style('display','none');
+            }
+        });
+    });
 }
 
 },{"../lib/custom_hash.js":150,"../lib/popup":152,"leaflet-geodesy":37,"qs-hash":51}],170:[function(require,module,exports){
