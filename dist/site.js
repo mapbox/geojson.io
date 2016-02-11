b206920d8f7315128b3a54b3e8f4ed2d741e353f
Tom MacWright <tom@macwright.org>
2016-02-11 15:51:43 -0500

    Force and allow https

--- dist/site.js
+++ dist/site.js
@@ -24755,6 +24755,7 @@ module.exports = function(hostname) {
     // Settings for geojson.io
     L.mapbox.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6IlpIdEpjOHcifQ.Cldl4wq_T5KOgxhLvbjE-w';
     if (hostname === 'geojson.io') {
+        L.mapbox.config.FORCE_HTTPS = true;
         return {
             client_id: '62c753fd0faf18392d85',
             gatekeeper_url: 'https://geojsonioauth.herokuapp.com'
