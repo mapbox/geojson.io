e0c79ad66a3cc9a78c8bef66b1a5e2b9bbbb5524
Tom MacWright <tom@macwright.org>
2017-02-19 11:40:29 -0500

    Remove geofenceapi.org integration
    Undoes https://github.com/mapbox/geojson.io/pull/527

--- src/ui/saver.js
+++ src/ui/saver.js
@@ -22,10 +22,6 @@ module.exports = function(context) {
             message = 'Changes committed to GitHub: ';
             url = res.commit.html_url;
             path = res.commit.sha.substring(0, 10);
-        } else if (type === 'geofenceapi' && res.type === 'geofenceapi') {
-            message = 'Geofence created.';
-            url = res.login_url;
-            path = 'Open geofence editor';
         } else {
             // Saved as a file
             message = 'Changes saved to disk.';
