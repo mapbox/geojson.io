911104d7c00b058dd22b6331f76b8f22d8efb49c
Tom MacWright <tom@macwright.org>
2018-05-13 18:32:26 -0700

    refactor: Use micro-auth instead of Gatekeeper: no more token request, hosting on now.

--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
 module.exports = function(hostname) {
     // Settings for geojson.io
     L.mapbox.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXFhYTA2bTMyeW44ZG0ybXBkMHkifQ.gUGbDOPUN1v1fTs5SeOR4A';
-    if (hostname === 'geojson.io') {
+    if (hostname === 'geojson.net') {
         L.mapbox.config.FORCE_HTTPS = true;
         return {
-            client_id: '62c753fd0faf18392d85',
-            gatekeeper_url: 'https://geojsonioauth.herokuapp.com'
+            authService: 'https://geojsonnet-auth.now.sh'
         };
     // Customize these settings for your own development/deployment
     // version of geojson.io.
@@ -16,8 +15,7 @@ module.exports = function(hostname) {
         L.mapbox.config.REQUIRE_ACCESS_TOKEN = true;
         return {
             GithubAPI: null,
-            client_id: 'bb7bbe70bd1f707125bc',
-            gatekeeper_url: 'https://localhostauth.herokuapp.com'
+            authService: 'https://geojsonnet-local-auth.now.sh'
         };
     }
 };
