f53254a291a5137a1f3fe6336e9a8a62bf7e8606
Chris Whong <chris.m.whong@gmail.com>
2022-10-20 09:26:01 -0400

    add osm raster tiles as a base layer option

--- dist/site.js
+++ dist/site.js
@@ -31711,6 +31711,32 @@ module.exports = function (context) {
         title: 'Dark',
         style: 'mapbox://styles/mapbox/dark-v10',
       },
+      {
+        title: 'OSM',
+        style: {
+          name: 'osm',
+          version: 8,
+          sources: {
+            'osm-raster-tiles': {
+              type: 'raster',
+              tiles: [
+                'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
+              ],
+              tileSize: 256,
+              attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+            }
+          }, 
+          layers: [
+            {
+              id: 'osm-raster-layer',
+              type: 'raster',
+              source: 'osm-raster-tiles',
+              minzoom: 0,
+              maxzoom: 22
+            }
+          ]
+        },
+      },
     ];
 
     var layerSwap = function (d) {
