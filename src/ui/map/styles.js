0a195336e921a221a1d06924ed888d2123c10917
jo <kaitokinjo@gmail.com>
2023-04-13 10:26:39 +0900

    add glyphs to style for OSM raster tile
    related issue #812

--- src/ui/map/styles.js
+++ src/ui/map/styles.js
@@ -24,6 +24,7 @@ module.exports = [
     style: {
       name: 'osm',
       version: 8,
+      glyphs: 'mapbox://fonts/mapbox/{fontstack}/{range}.pbf',
       sources: {
         'osm-raster-tiles': {
           type: 'raster',
