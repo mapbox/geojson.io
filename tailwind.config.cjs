4c1740beac3b186112a425c9e056a8685beaa77d
Andrew Sepic <andrew.sepic@mapbox.com>
2026-02-11 12:30:01 -0500

    Added button link

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,9 @@ module.exports = {
         'mb-purple': {
           500: '#4264fb',
           700: '#0f38bf'
-        }
+        },
+        'pink': '#d842fb',
+        'pink-dark': '#c23ee1'
       },
       fontFamily: {
         sans: ['"Open Sans"', 'sans-serif']
