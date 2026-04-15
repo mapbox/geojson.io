bf1f34dad2ec65c391ff7587b019321439970013
Andrew Sepic <andrew.sepic@mapbox.com>
2026-04-15 16:22:21 -0400

    Added circles

--- next/app/components/nothing_selected.tsx
+++ next/app/components/nothing_selected.tsx
@@ -1,29 +1,25 @@
 import {
   DotFilledIcon,
-  DownloadIcon,
   PlusIcon,
-  SquareIcon
+  SquareIcon,
+  CircleIcon
 } from '@radix-ui/react-icons';
 import Line from 'app/components/icons/line';
 import { useOpenFiles } from 'app/hooks/use_open_files';
 import { captureException } from 'integrations/errors';
-import { useSetAtom } from 'jotai';
 import { memo } from 'react';
-import { dialogAtom } from 'state/dialog_state';
-import { DialogHelpers } from 'state/dialog_helpers';
 import { Button } from './elements';
 import SvgPolygon from './icons/polygon';
 
 export const NothingSelected = memo(function NothingSelected() {
   const openFiles = useOpenFiles();
-  const setDialogState = useSetAtom(dialogAtom);
   return (
     <div className="px-3 pt-3 overflow-y-auto pb-4 text-gray-900 dark:text-gray-300 flex-auto geojsonio-scrollbar">
       <div className="text-sm font-semibold pb-2">
         Select a drawing tool from the menu
       </div>
       <div
-        className="grid gap-x-2 gap-y-4 items-start p-2 text-sm"
+        className="grid gap-x-2 gap-y-2 items-start p-2 text-sm"
         style={{
           gridTemplateColumns: 'min-content 1fr'
         }}
@@ -44,6 +40,10 @@ export const NothingSelected = memo(function NothingSelected() {
           <SquareIcon />
         </div>
         <div>Draw rectangles</div>
+        <div className="pt-1">
+          <CircleIcon />
+        </div>
+        <div>Draw circles</div>
       </div>
       <div className="pt-4 space-y-3">
         <div className="text-sm font-semibold">Import a file</div>
